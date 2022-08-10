060c06fec3c83dc94916f48e347b092ea9a3e712
seung <99049141+kimseunghyo@users.noreply.github.com>
2022-08-10 23:48:45 +0900

    Time: 189 ms (69.01%), Space: 19.5 MB (69.37%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        r = 0
+        c = len(matrix[0]) - 1
+        
+        while r < len(matrix) and c >= 0:
+            if matrix[r][c] == target:
+                return True
+            
+            elif matrix[r][c] < target:
+                r += 1
+                
+            else:
+                c -= 1
+                
+        return False
+            
