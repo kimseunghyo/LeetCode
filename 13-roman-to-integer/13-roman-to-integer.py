4e4fb88b38db475cd2e4ba80c590587eca093edc
seung <99049141+kimseunghyo@users.noreply.github.com>
2022-08-17 00:40:34 +0900

    Time: 91 ms (12.79%), Space: 13.6 MB (8.59%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,46 +1,14 @@
 class Solution(object):
     def romanToInt(self, s):
-        value = 0
+        dict_s = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        integer = 0
         
         for i in range(len(s)):
-            if s[i] == 'I':
-                value += 1
-                
-            elif s[i] == 'V': 
-                if s[i - 1] == 'I' and i > 0:
-                    value += 3
-                else:
-                    value += 5
-                
-            elif s[i] == 'X': 
-                if s[i - 1] == 'I' and i > 0:
-                    value += 8
-                else:
-                    value += 10
-                
-            elif s[i] == 'L': 
-                if s[i - 1] == 'X' and i > 0:
-                    value += 30
-                else:
-                    value += 50
-                
-            elif s[i] == 'C': 
-                if s[i - 1] == 'X' and i > 0:
-                    value += 80
-                else:
-                    value += 100
-                
-            elif s[i] == 'D': 
-                if s[i - 1] == 'C' and i > 0:
-                    value += 300
-                else:
-                    value += 500
-                
-            elif s[i] == 'M': 
-                if s[i - 1] == 'C' and i > 0:
-                    value += 800
-                else:
-                    value += 1000
+            if dict_s[s[i - 1]] < dict_s[s[i]] and i > 0:
+                integer += dict_s[s[i]] - dict_s[s[i - 1]] * 2
+            
+            else:
+                integer += dict_s[s[i]]
                     
-        return value
+        return integer
                 
