7235d945c222aee2ec38930181921d8ff62efe86
seung <99049141+kimseunghyo@users.noreply.github.com>
2022-09-04 22:42:00 +0900

    Time: 210 ms (8.32%), Space: 13.1 MB (96.83%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -3,14 +3,16 @@ class Solution(object):
         if n <= 0:
             return False
         
-        a = 0
-        
-        while n >= 1:
-            if a == 0 and n == 1:
+        else:
+            a = 0
+            
+            while n > 1:
+                a += n % 3
+                n = n // 3
+                
+            if a == 0:
                 return True
             
-            a += n % 3
-            n = n // 3
-            
-        return False
+            else:
+                return False
         
