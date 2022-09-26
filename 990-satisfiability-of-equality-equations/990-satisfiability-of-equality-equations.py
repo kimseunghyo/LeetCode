63e77df7b648a9420b6d003d526810c2f878fb9e
seung <99049141+kimseunghyo@users.noreply.github.com>
2022-09-26 22:58:33 +0900

    Time: 33 ms (84.21%), Space: 13.9 MB (7.89%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def equationsPossible(self, equations):
+        uf = {}
+        
+        def find(x):
+            uf.setdefault(x, x)
+            
+            if x != uf[x]:
+                uf[x] = find(uf[x])
+                
+            return uf[x]
+        
+        def union(x, y):
+            uf[find(x)] = find(y)
+            
+        
+        for e in equations:
+            if e[1:3] == '==':
+                union(e[0], e[-1])
+                
+        for e in equations:
+            if e[1:3] == '!=':
+                if find(e[0]) == find(e[-1]):
+                    return False
+                
+        return True
+        
