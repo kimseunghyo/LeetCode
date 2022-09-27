a3aef1aa0e57464637e48fed76cff987fb30ce95
seung <99049141+kimseunghyo@users.noreply.github.com>
2022-09-27 23:52:08 +0900

    Time: 2489 ms (19.39%), Space: 18.3 MB (40.17%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def wordSubsets(self, words1, words2):
+        res = []
+        subsets = defaultdict(int)
+        
+        for w2 in words2:
+            cnt_w2 = Counter(w2)
+            
+            for key in cnt_w2:
+                subsets[key] = max(subsets[key], cnt_w2[key])
+                
+        for w1 in words1:
+            cnt_w1 = Counter(w1)
+            temp = True
+            
+            for key in subsets:
+                if key not in cnt_w1 or cnt_w1[key] < subsets[key]:
+                    temp = False
+                    
+                    break
+                    
+            if temp:
+                res.append(w1)
+            
+        return res
