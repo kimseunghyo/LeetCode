34e2f60e0103b9a38b4dee1c4d23e08a0e23543a
seung <99049141+kimseunghyo@users.noreply.github.com>
2022-12-09 22:38:03 +0900

    Time: 58 ms (29.06%), Space: 19.7 MB (67.52%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxAncestorDiff(self, root):
+        def dfs(root, max_n, min_n):
+            if root is None:
+                return
+            
+            if root.val >= max_n:
+                max_n = root.val
+                
+            if root.val <= min_n:
+                min_n = root.val
+                
+            if max_n - min_n >= self.res:
+                self.res = max_n - min_n
+            
+            dfs(root.left, max_n, min_n)
+            dfs(root.right, max_n, min_n)
+            
+        
+        self.res = 0
+        
+        dfs(root, root.val, root.val)
+        
+        return self.res
